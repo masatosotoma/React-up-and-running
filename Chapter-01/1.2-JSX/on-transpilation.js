c8339a20fe758d3f8ff881d4ba366a28fb974f83
masatosotoma <masatosotoma@gmail.com>
2022-07-31 20:43:23 -0400

    Explained transpilation

---
+++ Chapter-01/1.2-JSX/on-transpilation.js
@@ -0,0 +1,24 @@
+/*
+JSX is a separate technology from React and is completely optional.
+You can opt into never coming anywhere near JSX at all. 
+But it’s very likely that once you try it, you won’t go back to function calls.
+*/
+
+/*
+The process of transpilation is a process of taking source code and rewriting it to
+accomplish the same results but using syntax that’s understood by older browsers. 
+*/
+
+/*
+A polyfill is a solution in pure JavaScript-land. 
+It’s a good solution when adding new methods to existing objects or implementing new objects (such as JSON)
+
+But it’s not sufficient when new syntax is introduced into the language. 
+Any new syntax in the eyes of a browser that does not support it is just invalid and throws a parse error
+*/
+
+/*
+Transpiling JavaScript is getting more and more common  as programmers want to use the latest JavaScript (ECMAScript) features 
+without waiting for browsers to implement them
+*/
+
