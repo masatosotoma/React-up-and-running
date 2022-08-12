6771417346445144d158512d304a8ca76968e5e1
masatosotoma <masatosotoma@gmail.com>
2022-08-11 23:06:20 -0400

    Explained properties vs state

---
+++ Chapter-02/1.8-props-vs-state/props-vs-state.js
@@ -0,0 +1,13 @@
+
+/*
+
+Properties are a mechanism for the outside world (users of the component) to configure your component. 
+State is your internal data maintenance. 
+
+if you consider an analogy with object-oriented programming, 
+this.props is like a collection of all the arguments passed to a class constructor, 
+while this.state is a bag of your private properties.
+
+In general, prefer to split your application in a way that you have fewer stateful components and more stateless ones.
+
+*/
