2b9d9c063957a02c726b676c215bccddf5b23180
masatosotoma <masatosotoma@gmail.com>
2022-09-20 07:08:43 -0400

    Explained how to improve components

---
+++ Chapter-03/3.4-adding-td-content/improve-the-component.js
@@ -0,0 +1,8 @@
+/*
+
+Allowing only string data is a bit too restrictive for a generic Microsoft Excel spreadsheet.
+As an exercise for your own amusement, you can change this example to allow
+more data types (PropTypes.any) and then render differently depending on the type
+(e.g., align numbers to the right).
+
+*/
