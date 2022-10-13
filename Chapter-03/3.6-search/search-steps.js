a473add16e4f594fdc732202b6faf7c71f2f1ff9
masatosotoma <masatosotoma@gmail.com>
2022-10-12 21:57:06 -0400

    Explained the steps of adding search feature

---
+++ Chapter-03/3.6-search/search-steps.js
@@ -0,0 +1,9 @@
+/*
+Add a search feature to the Excel component that allows users to filter the contents of the table
+*/
+
+//1. Add a button to toggle the new feature on and off
+
+//2. If the search is on, add a row of inputs where each one searches in the corresponding column
+
+//3. As a user types in an input box, filter the array of state.data to show only the matching content
