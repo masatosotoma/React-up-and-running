b11aeaa9df2b2d2128c99da387f1ebd343a4f673
masatosotoma <masatosotoma@gmail.com>
2023-07-20 20:17:43 -0400

    Explained about how reducer func works

---
+++ Chapter-04/4.8-useReduer/reducer-function.js
@@ -0,0 +1,17 @@
+//Reducer function simple form
+function myReducer(oldState, action) {
+  const newState = {};
+  // do something with `oldState` and `action`;
+  return newState;
+}
+
+/*The reducer function is responsible for making sense of the reality 
+when something happens in the world. The world is a mess, then an event happens. 
+The function that should makeSense() of the world reconciles the mess with the new event 
+and reduces all the complexity to a nice state or order: */
+
+function makeSense(mess, event) {
+  const order = {};
+  // do something with mess and event
+  return order;
+}
