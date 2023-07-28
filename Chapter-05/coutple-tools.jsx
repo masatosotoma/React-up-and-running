b50bd9346f169c943eb2cb12162a4f213ee23065
masatosotoma <masatosotoma@gmail.com>
2023-07-27 22:07:23 -0400

    JSX tools explained

---
+++ Chapter-05/coutple-tools.jsx
@@ -0,0 +1,18 @@
+/*
+To experiment and get familiar with the JSX transforms, 
+you can play with the live editor at https://babeljs.io/repl 
+*/
+
+ReactDOM.render(
+  <h1 id="my-heading">
+    <span>
+      Hello <em>Wonderful</em>
+    </span>{" "}
+    World!
+  </h1>,
+  document.getElementById("app")
+);
+
+
+/*Another online tool you may find helpful when learning JSX or 
+transitioning an existing app’s markup from HTML is the HTML-to-JSX compiler*/
