c132546899e8d5b2a02c30ab384abc47e9fa6f01
masatosotoma <masatosotoma@gmail.com>
2023-07-30 10:38:03 -0400

    Explained about comments on JSX

---
+++ Chapter-05/1.1-jsx/comments-on-jsx.jsx
@@ -0,0 +1,17 @@
+/*Because the expressions wrapped in {} are just JavaScript, 
+you can easily add multi‐ line comments using /* comment */
+/*You can also add single-line comments using // comment, 
+but you have to make sure the closing } of the expression is on a separate line so it’s not considered part of the comment:*/
+
+<h1>
+    {/* miltiline comment*/}
+    {/*
+    multi
+    line
+    comment
+    */}
+    {
+        //single line
+    }
+    Hello!
+</h1>
