ca4bbc659d98ff6f0f01e5fc5f8714fe34b36f24
masatosotoma <masatosotoma@gmail.com>
2023-08-24 22:15:05 -0400

    Checked the node.js version

---
+++ Chapter-06/6.1-Create-React-App/node.js
@@ -0,0 +1,14 @@
+/*
+
+To install Node.js, go to https://nodejs.org and grab the installer for your operating 
+system. Follow the instructions of the installer and it’s all done. Now you can avail yourself of the services provided by the command-line Node package manager (npm) utility.
+Even if you have Node.js installed, 
+it’s a good idea to make sure you have the latest version.
+To verify, type this in your terminal:
+
+$ npm --version
+
+masatosotoma@MASATOs-MacBook-Pro ~ % npm --version
+9.5.0
+
+*/
