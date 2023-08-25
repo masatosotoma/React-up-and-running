1f2162b4535dd7514ccace58ad56cae51b6b71a0
masatosotoma <masatosotoma@gmail.com>
2023-08-24 22:19:14 -0400

    How to install Create React App (CRA)

---
+++ Chapter-06/hello-cra.js
@@ -0,0 +1,14 @@
+/*
+You can install CRA and have it available locally for future projects. 
+
+convenient approach is to use the npx utility that comes with Node.js. 
+It allows you to execute (hence the “x”) Node package scripts. 
+You can run the CRA script once: it downloads and executes the last version, 
+sets up your app, and it’s gone.
+
+To get started, create a temporary directory and execute CRA:
+$ mkdir ~/reactbook/test
+$ cd ~/reactbook/test
+$ npx create-react-app hello
+
+*/
