7981e6abe360f50ba087280449fb39de1906d9f6
masatosotoma <masatosotoma@gmail.com>
2023-08-27 22:38:16 -0400

    Explained how to start a new app in the terminal

---
+++ Chapter-06/6.1-Create-React-App/build-and-deploy.js
@@ -0,0 +1,18 @@
+/*
+Yyou’re ready to unleash the new app into the world. 
+Go to the terminal/console window and press Ctrl + C. 
+This kills the pro‐ cess and further changes will not auto-update in the browser. 
+You’re ready. Type the following:
+
+npm run build
+
+This is the process of building and packaging the app, 
+ready to be deployed. The build is found in a /build folder 
+
+When you want to make a change, repeat the process:
+
+$ npm start
+// work, work, work... // Ctrl+C
+$ npm run build
+
+*/
