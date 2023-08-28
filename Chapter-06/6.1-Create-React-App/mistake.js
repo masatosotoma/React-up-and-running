5e151591edcd10831612f04cb8f7bb14bbf4fe2e
masatosotoma <masatosotoma@gmail.com>
2023-08-27 22:39:25 -0400

    Explained what will happen when there's a mistake

---
+++ Chapter-06/6.1-Create-React-App/mistake.js
@@ -0,0 +1,6 @@
+/*
+
+When you save a file with an error in it (maybe you forgot to close a JSX tag), 
+the ongoing build fails and you get an error message in both the console and the browser.
+
+*/
