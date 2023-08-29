5f66cea2cbe20769dff80946ec0182be72b4b892
masatosotoma <masatosotoma@gmail.com>
2023-08-28 22:59:27 -0400

    Explained about the node modules

---
+++ Chapter-06/6.1-Create-React-App/package.json-and-nodemodules.js
@@ -0,0 +1,12 @@
+/*
+One of the configuration pieces deals with dependencies, 
+such as React and React-DOM. These dependencies are installed 
+in the node_modules folder in the root of the app
+
+The dependencies there are for developing and building the app, not for deployment. 
+And they should not be included if you share the code of your app with friends, 
+coworkers or the open-source community
+
+When someone else wants to contribute or you want to contribute to another app, 
+you install the dependencies locally.
+*/
