015b4fc8e60d1d2914a6a83f4e9fc99cb0461b99
masatosotoma <masatosotoma@gmail.com>
2023-08-29 23:06:14 -0400

    Explained the index and whre id=root defined

---
+++ Chapter-06/6.2-poking-around-code/indice.js
@@ -0,0 +1,14 @@
+/*
+In public/index.html you’ll find the old school HTML index page, 
+the root of every‐ thing rendered by the browser. 
+This is where <div id="root"> is defined and 
+where React will render your top level component and all its children.
+The file src/index.js is the main entry for the app as far as React is concerned. 
+
+Note the top part:
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
