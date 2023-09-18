90af50701ea4b659a8a7eaa993af1704fb444568
masatosotoma <masatosotoma@gmail.com>
2023-09-17 21:25:00 -0400

    Explained CSS installation

---
+++ Chapter-06/6.2-poking-around-code/css.js
@@ -0,0 +1,22 @@
+import "./index.css";
+
+/*
+The src/index.css should contain general styles, 
+such as body, html, and so on, that are applicable to the whole page.
+*/
+
+/*
+Good idea could be to prefix all class names used in MyComponent.js 
+with MyComponent-. For example:*/
+
+import "./index.css";
+
+/*
+.MyComponent-table{
+    border:1px solid black;
+}
+*/
+/*
+.MyComponent-table-heading { border: 1px solid black;
+}
+*/
