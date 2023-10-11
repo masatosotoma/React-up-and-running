e9e542f32961e429a2d324661d7bdbd30055cd6a
masatosotoma <masatosotoma@gmail.com>
2023-10-10 23:35:05 -0400

    Created the discovery function js file

---
+++ Chapter-07/whinepad/src/components/Discovery.js
@@ -0,0 +1,17 @@
+import Excel from "./Excel";
+
+function Discovery() {
+  return (
+    <div>
+      <h2>Excel</h2>
+      <Excel
+        headers={["Name", "Year"]}
+        initialData={[
+          ["Charles", "1859"],
+          ["Antoine", "1943"],
+        ]}
+      />
+      {/* more components here */}{" "}
+    </div>
+  );
+}
