74efe3d6e6fd905b2b72459e949d23818a42c386
masatosotoma <masatosotoma@gmail.com>
2023-10-10 22:44:13 -0400

    Updated contents of titles etc

--- Chapter-07/whinepad/src/App.js
+++ Chapter-07/whinepad/src/App.js
@@ -1,23 +1,13 @@
 import "./App.css";
 import Excel from "./components/Excel";
 
-const headers = localStorage.getItem('headers')
-const data = localStorage.getItem("data");
-if (!headers) {
-  headers = ["Title", "Year", "Rating", "Comments"];
-  data = [["Red whine", "2021", "3", "meh"]];
-}
-
 function App() {
   return (
     <div className="Excel">
       {" "}
       <Excel
-        headers={["Name", "Year"]}
-        initialData={[
-          ["Charles", "1859"],
-          ["Antoine", "1943"],
-        ]}
+        headers={["Title", "Year", "Rating", "Comments"]}
+        initialData={[["Red whine", "2021", "3", "meh"]]}
       />{" "}
     </div>
   );
