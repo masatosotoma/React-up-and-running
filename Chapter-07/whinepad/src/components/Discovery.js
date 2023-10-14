b3e6bf46e7d508f5ecfe0ea866c41e41db21fd47
masatosotoma <masatosotoma@gmail.com>
2023-10-14 13:01:40 -0400

    Updated Discovery.js adding Logo+Body

--- Chapter-07/whinepad/src/components/Discovery.js
+++ Chapter-07/whinepad/src/components/Discovery.js
@@ -1,17 +1,18 @@
 import Excel from "./Excel";
+import Header from '/Header';
+import Body from "./Body";
+import Logo from "./Logo";
 
 function Discovery() {
   return (
-    <div>
-      <h2>Excel</h2>
-      <Excel
-        headers={["Name", "Year"]}
-        initialData={[
-          ["Charles", "1859"],
-          ["Antoine", "1943"],
-        ]}
-      />
-      {/* more components here */}{" "}
+    <div className="Discovery">
+        <h2>Logo</h2>
+        <div style={{background: '#f6f6f6', display: 'inline-block'}}>
+            <Logo />
+        </div>
+        <h2>Body</h2>
+        <Body>I am content inside the body</Body>
+
     </div>
   );
 }
