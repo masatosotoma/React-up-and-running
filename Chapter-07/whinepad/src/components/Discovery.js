885009b0734cf66b17fb70b1aa85be33430d0377
masatosotoma <masatosotoma@gmail.com>
2023-10-16 23:09:11 -0400

    Created the button component

--- Chapter-07/whinepad/src/components/Discovery.js
+++ Chapter-07/whinepad/src/components/Discovery.js
@@ -1,20 +1,31 @@
 import Excel from "./Excel";
-import Header from '/Header';
+import Header from "/Header";
 import Body from "./Body";
 import Logo from "./Logo";
+import Button from "./Button";
 
 function Discovery() {
   return (
     <div className="Discovery">
-        <h2>Logo</h2>
-        <div style={{background: '#f6f6f6', display: 'inline-block'}}>
-            <Logo />
-        </div>
-        <h2>Body</h2>
-        <Body>I am content inside the body</Body>
-
+      <h2>Logo</h2>
+      <div style={{ background: "#f6f6f6", display: "inline-block" }}>
+        <Logo />
+      </div>
+      <h2>Body</h2>
+      <Body>I am content inside the body</Body>
+      <p>
+        Button with onClick:{" "}
+        <Button onClick={() => alert("ouch")}>Click me</Button>
+      </p>
+      <p>
+        A link: <Button href="https://reactjs.org/">Follow me</Button>
+      </p>{" "}
+      <p>
+        Custom class name:{" "}
+        <Button className="Discovery-custom-button">I do nothing</Button>{" "}
+      </p>
     </div>
   );
 }
 
-export default Discovery
+export default Discovery;
